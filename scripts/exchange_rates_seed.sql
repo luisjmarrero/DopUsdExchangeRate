25b43d3d225fc938af0de53ae867b72a4e0145ca
Luis Marrero <luijsmarrerop@gmail.com>
2025-08-23 01:23:21 -0400

    fix backups and update seed data

--- scripts/exchange_rates_seed.sql
+++ scripts/exchange_rates_seed.sql
@@ -1,6 +1,18 @@
-INSERT INTO exchange_rates (id, bank, buy_rate, sell_rate, sync_date, source) VALUES (
-    29, 'Banreservas', 62.55, 60.55, '2025-08-20 12:18:21+0000', 'https://www.banreservas.com/');
-INSERT INTO exchange_rates (id, bank, buy_rate, sell_rate, sync_date, source) VALUES (
-    30, 'BHD', 62.65, 59.95, '2025-08-20 12:18:30+0000', 'https://bhd.com.do/not-found');
-INSERT INTO exchange_rates (id, bank, buy_rate, sell_rate, sync_date, source) VALUES (
-    31, 'Scotiabank', 62.65, 60.0, '2025-08-20 12:18:31+0000', 'https://do.scotiabank.com/banca-personal/tarifas/tasas-de-cambio.html');
+INSERT INTO exchange_rates (id, bank, buy_rate, sell_rate, sync_date, source, sell_change, buy_change) VALUES (
+    29, 'Banreservas', 62.55, 60.55, '2025-08-20 12:18:21+0000', 'https://www.banreservas.com/', NULL, NULL);
+INSERT INTO exchange_rates (id, bank, buy_rate, sell_rate, sync_date, source, sell_change, buy_change) VALUES (
+    32, 'Banreservas', 62.75, 60.75, '2025-08-21 12:50:09+0000', 'https://www.banreservas.com/', 0.20000000000000284, 0.20000000000000284);
+INSERT INTO exchange_rates (id, bank, buy_rate, sell_rate, sync_date, source, sell_change, buy_change) VALUES (
+    38, 'Banreservas', 62.85, 60.85, '2025-08-22 12:00:01+0000', 'https://www.banreservas.com/', 0.10000000000000142, 0.10000000000000142);
+INSERT INTO exchange_rates (id, bank, buy_rate, sell_rate, sync_date, source, sell_change, buy_change) VALUES (
+    30, 'BHD', 62.65, 59.95, '2025-08-20 12:18:30+0000', 'https://bhd.com.do/not-found', NULL, NULL);
+INSERT INTO exchange_rates (id, bank, buy_rate, sell_rate, sync_date, source, sell_change, buy_change) VALUES (
+    33, 'BHD', 62.8, 60.0, '2025-08-21 12:50:19+0000', 'https://bhd.com.do/not-found', 0.04999999999999716, 0.14999999999999858);
+INSERT INTO exchange_rates (id, bank, buy_rate, sell_rate, sync_date, source, sell_change, buy_change) VALUES (
+    39, 'BHD', 62.9, 60.1, '2025-08-22 12:00:11+0000', 'https://bhd.com.do/not-found', 0.10000000000000142, 0.10000000000000142);
+INSERT INTO exchange_rates (id, bank, buy_rate, sell_rate, sync_date, source, sell_change, buy_change) VALUES (
+    31, 'Scotiabank', 62.65, 60.0, '2025-08-20 12:18:31+0000', 'https://do.scotiabank.com/banca-personal/tarifas/tasas-de-cambio.html', NULL, NULL);
+INSERT INTO exchange_rates (id, bank, buy_rate, sell_rate, sync_date, source, sell_change, buy_change) VALUES (
+    34, 'Scotiabank', 62.8, 60.15, '2025-08-21 12:50:19+0000', 'https://do.scotiabank.com/banca-personal/tarifas/tasas-de-cambio.html', 0.14999999999999858, 0.14999999999999858);
+INSERT INTO exchange_rates (id, bank, buy_rate, sell_rate, sync_date, source, sell_change, buy_change) VALUES (
+    40, 'Scotiabank', 62.9, 60.25, '2025-08-22 12:00:12+0000', 'https://do.scotiabank.com/banca-personal/tarifas/tasas-de-cambio.html', 0.10000000000000142, 0.10000000000000142);
